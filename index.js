b7a997726a2946dc51904482e81fe0a695b68142
Shyalenn Cerolin <s2200132@student.unklab.ac.id>
2022-06-27 08:20:30 +0800

    tambah file login.js

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ console.log("baris keempat");
 //kamis 
 console.log("baris kelima");
 
-<<<<<<< HEAD
-//jumaat 
-console.log("baris keenam");
-console.log("baris ketujuh");
-=======
 //jumaat
 console.log("baris keenam");
 console.log("baris ketujuh");
->>>>>>> 274f9e7b738b2ac5019aee99cc6667753d06e112
